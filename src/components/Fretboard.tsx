c7596c33de1e8ded04f8496e9bc290d591c0de21
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-08-30 23:04:03 +0900

    [#10] refactor: 크로매틱 스케일 연주 구현 로직 변경
    - 스케일은 null값을 포함한 2차원 배열로 제공
    - null 값이면 스케일에 포함되지 않는 노트이므로 재생할 때는 null이 아닌 노트만 추출
    - 순서대로 재생 및 currentPlayingNotes 업데이트
    - currentPlayingNotes: boolean 자료형 2차원 배열. 재생중인 노트의 위치를 true값으로 관리 => Fretboard로 전달하여 UI에 반영

--- src/components/Fretboard.tsx
+++ src/components/Fretboard.tsx
@@ -1,22 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import { fretboard, openNotes } from "../data/scales/Fretboard";
 
 const GuitarContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
 `;
-
-const fretboard = [
-    ["F2", "F#2", "G2", "G#2", "A2", "A#2", "B2", "C3", "C#3", "D3", "D#3", "E3"], // 6번 줄
-    ["A#2", "B2", "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3"], // 5번 줄
-    ["D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", "D4"], // 4번 줄
-    ["G#3", "A3", "A#3", "B3", "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4"], // 3번 줄
-    ["C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4"], // 2번 줄
-    ["F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", "C5", "C#5", "D5", "D#5", "E5"], // 1번 줄
-];
-
-const openNotes = ["E2", "A2", "D3", "G3", "B3", "E4"];
 
 const FretboardContainer = styled.div`
     display: flex;
@@ -72,21 +62,10 @@ const Note = styled.div<{ isActive?: boolean; isOpenNote?: boolean }>`
 `;
 
 interface FretboardProps {
-    currentNote: string | null;
-    playedNotesCount: number;
+   currentPlayingNotes: boolean[][];
 }
 
-const getNotePosition = (note: string | null, playedNotesCount: number) => {
-    if (!note) return { stringIndex: -1, fretIndex: -1 };
-
-    const stringIndex = Math.floor(playedNotesCount / 12); // 12개씩 묶어서 줄을 결정
-    const fretIndex = playedNotesCount % 12; // 12로 나눈 나머지 값이 프렛 인덱스
-
-    return { stringIndex, fretIndex };
-};
-
-const Fretboard: React.FC<FretboardProps> = ({ currentNote, playedNotesCount }) => {
-    const { stringIndex, fretIndex } = getNotePosition(currentNote, playedNotesCount);
+const Fretboard: React.FC<FretboardProps> = ({currentPlayingNotes }) => {
 
     return (
         <GuitarContainer>
@@ -98,10 +77,10 @@ const Fretboard: React.FC<FretboardProps> = ({ currentNote, playedNotesCount }) 
                 ))}
             </OpenNotesContainer>
             <FretboardContainer>
-                {fretboard.map((string, sIndex) => (
-                    <NoteRow key={sIndex}>
-                        {string.map((note, fIndex) => (
-                            <Note key={fIndex + 1} isActive={sIndex === stringIndex && fIndex === fretIndex}>
+                {fretboard.map((row, rowIndex) => (
+                    <NoteRow key={rowIndex}>
+                        {row.map((note, colIndex) => (
+                            <Note key={colIndex + 1}  isActive={currentPlayingNotes[rowIndex][colIndex]}>
                                 <div>{note.replace(/[0-9]/g, '')}</div>
                             </Note>
                         ))}
