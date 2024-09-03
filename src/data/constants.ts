219cc30eb52cbd9f769fb62902e31988a3fbac74
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-09-04 01:55:09 +0900

    [#3] feat: settings로 전달받은 scale명에 해당하는 스케일 재생 (Chromatic or Test)

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,3 +1,3 @@
-export const scales = ["Chromatic", "Major", "Minor", "Major Pentatonic", "Minor Pentatonic"];
+export const scales = ["Chromatic","Test", "Major", "Minor", "Major Pentatonic", "Minor Pentatonic"];
 
 export const keys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
