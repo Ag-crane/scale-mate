83111ac2ce5418ad67e27c64b53c18bc24ccafc6
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-09-04 02:15:48 +0900

    refactor: export문 통합

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,8 +1,8 @@
-export const scales = ["Chromatic","Test", "Major", "Minor", "Major Pentatonic", "Minor Pentatonic"];
+const scales = ["Chromatic","Test", "Major", "Minor", "Major Pentatonic", "Minor Pentatonic"];
 
-export const keys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+const keys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 
-export const fretboard = [
+const fretboard = [
     ["F2", "F#2", "G2", "G#2", "A2", "A#2", "B2", "C3", "C#3", "D3", "D#3", "E3"], // 6번 줄
     ["A#2", "B2", "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3"], // 5번 줄
     ["D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", "D4"], // 4번 줄
@@ -11,4 +11,6 @@ export const fretboard = [
     ["F4", "F#4", "G4", "G#4", "A4", "A#4", "B4", "C5", "C#5", "D5", "D#5", "E5"], // 1번 줄
 ];
 
-export const openNotes = ["E2", "A2", "D3", "G3", "B3", "E4"];
+const openNotes = ["E2", "A2", "D3", "G3", "B3", "E4"];
+
+export { scales, keys, fretboard, openNotes };
