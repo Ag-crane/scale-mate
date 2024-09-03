5ddf4ad0e4d2846a22eb270c9defd27fa3729de4
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-09-03 22:44:08 +0900

    refactor: 스타일링 코드를 별도의 .styles.ts 파일로 분리

--- src/components/Fretboard.tsx
+++ src/components/Fretboard.tsx
@@ -1,65 +1,6 @@
 import React from "react";
-import styled from "styled-components";
 import { fretboard, openNotes } from "../data/scales/Fretboard";
-
-const GuitarContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
-
-const FretboardContainer = styled.div`
-    display: flex;
-    flex-direction: column-reverse;
-    border: 2px solid black;
-    width: 950px;
-    height: 240px;
-    margin: 20px;
-    padding: 10px 0px;
-    background-color: #a0522d;
-    border-radius: 10px;
-`;
-
-const OpenNotesContainer = styled.div`
-    display: flex;
-    flex-direction: column-reverse;
-    margin-right: 10px;
-    height: 240px;
-`;
-
-const NoteRow = styled.div`
-    display: flex;
-    height: 40px;
-`;
-
-const Note = styled.div<{ isActive?: boolean; isOpenNote?: boolean }>`
-    flex: 1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    color: black;
-    font-weight: bold;
-    font-size: 12px;
-
-    & > div {
-        background-color: ${({ isActive, isOpenNote }) =>
-            isActive ? "#ff0" : isOpenNote ? "#753c24" : "#fff"};
-        color: ${({ isActive, isOpenNote }) => isActive ? "#000" : isOpenNote ? "#fff" : "#333"};
-        border-radius: 50%;
-        width: 28px;
-        height: 28px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    } 
-
-    border-right: ${({ isOpenNote }) => (isOpenNote ? "none" : "2px solid silver")};
-
-    &:last-child {
-        border-right: none;
-    }
-`;
+import { FretboardContainer, GuitarContainer, Note, NoteRow, OpenNotesContainer } from "./Fretboard.styles";
 
 interface FretboardProps {
    currentPlayingNotes: boolean[][];
---
+++ src/components/SettingBoard.styles.ts
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    padding: 20px;
+    max-width: 400px;
+    margin: 0 auto;
+    text-align: center;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    background-color: #f9f9f9;
+`;
+
+const Label = styled.label`
+    margin-right: 10px;
+    font-size: 16px;
+    font-weight: bold;
+`;
+
+const Select = styled.select`
+    padding: 5px;
+    font-size: 16px;
+`;
+
+const Input = styled.input`
+    padding: 5px;
+    font-size: 16px;
+    width: 60px;
+    text-align: center;
+`;
+
+const Button = styled.button`
+    margin-top: 20px;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    border: none;
+    border-radius: 5px;
+    background-color: rgb(40, 44, 52, 0.5);
+    color: white;
+
+    &:hover {
+        background-color: rgb(40, 44, 52, 1);
+    }
+`;
+
+export { Container, Label, Select, Input, Button };
