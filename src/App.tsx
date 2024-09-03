033db39dd26c6de7213bc94980a7fefc3a4c4001
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-09-03 23:46:08 +0900

    refactor: App.tsx 스타일링 코드 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,7 @@
 import React, { useState } from "react";
 import SettingBoard from "./components/SettingBoard";
 import ScalePlayer from "./components/ScalePlayer";
-import styled from "styled-components";
-
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    min-height: 100vh;
-    background-color: #f0f2f5;
-    padding: 20px;
-    padding-top: 60px; /* Header의 높이만큼 패딩 추가 */
-`;
-
-const SettingBoardContainer = styled.div`
-    margin-top: 20px;
-    width: 100%;
-    max-width: 1200px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    border-radius: 10px;
-    background-color: #ffffff;
-    padding: 20px;
-`;
-
-const ScalePlayerContainer = styled.div`
-    margin-top: 20px;
-    width: 100%;
-    max-width: 1200px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    border-radius: 10px;
-    background-color: #ffffff;
-    padding: 20px;
-`;
-
-const Header = styled.div`
-    background-color: #282c34;
-    height: 80px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: white;
-    width: 100%;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 1000;
-`;
+import { Container, Header, ScalePlayerContainer, SettingBoardContainer } from "./App.styles";
 
 const App: React.FC = () => {
     const [settings, setSettings] = useState({
