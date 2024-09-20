5071ce3881c4c06b839648182eba746f10d34a44
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-09-20 11:41:03 +0900

    [#16] feat: settings.key를 Fretboard에 전달, 으뜸음 강조 표시

--- src/components/Fretboard.tsx
+++ src/components/Fretboard.tsx
@@ -5,9 +5,10 @@ import { FretboardContainer, GuitarContainer, Note, NoteRow, OpenNotesContainer 
 interface FretboardProps {
    currentPlayingNotes: boolean[][];
    scaleNotes: (string | null)[][]; // 선택한 스케일에 따른 프렛보드 노트 배열
+   rootNote: string;                // 으뜸음 (Key)
 }
 
-const Fretboard: React.FC<FretboardProps> = ({currentPlayingNotes, scaleNotes }) => {
+const Fretboard: React.FC<FretboardProps> = ({currentPlayingNotes, scaleNotes, rootNote }) => {
 
     return (
         <GuitarContainer>
@@ -24,12 +25,14 @@ const Fretboard: React.FC<FretboardProps> = ({currentPlayingNotes, scaleNotes })
                         {row.map((note: string, colIndex: number) => {
                             const isActive = currentPlayingNotes[rowIndex][colIndex]; // 현재 재생 중인 노트
                             const isScaleNote = !!scaleNotes[rowIndex][colIndex];    // 스케일에 속하는 노트
+                            const isRootNote = scaleNotes[rowIndex][colIndex]?.startsWith(rootNote); // 으뜸음 확인
 
                             return (
                                 <Note 
                                     key={colIndex + 1}  
                                     isActive={isActive}
                                     isScaleNote={isScaleNote}
+                                    isRootNote={isRootNote}
                                 >
                                     <div>{note.replace(/[0-9]/g, '')}</div>
                                 </Note>
