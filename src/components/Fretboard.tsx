2ead13501730c9259a1c2895318376eaddc255a6
Ag-crane <enaklee401@dongguk.edu>
2024-10-08 13:41:01 +0900

    [#22] feat: BlockNumbers 계산하여 Fretboard로 전달하고 UI 표시
    - 각 노트가 포함되는 블록의 번호를 각 노트의 위치에 배열로 저장하여 Fretboard로 전달
    - 노트의 투명도를 조절하여 시각적으로 구분 : 스케일에 해당되지 않는 부분은 0, 스케일에 해당하고 블록에 해당되지 않는 노트는 0.3, 모두 해당하는 노트는 1

--- src/components/Fretboard.tsx
+++ src/components/Fretboard.tsx
@@ -1,38 +1,43 @@
 import React from "react";
 import { fretboard, openNotes } from "../data/constants";
-import { FretboardContainer, GuitarContainer, Note, NoteRow, OpenNotesContainer } from "./Fretboard.styles";
+import { FretboardContainer, GuitarContainer, Note, NoteRow, OpenNote, OpenNotesContainer } from "./Fretboard.styles";
 
 interface FretboardProps {
    currentPlayingNotes: boolean[][];
-   scaleNotes: (string | null)[][]; // 선택한 스케일에 따른 프렛보드 노트 배열
-   rootNote: string;                // 으뜸음 (Key)
+   scaleNotes: (string | null)[][];
+   rootNote: string;
+   selectedBlock: number | null;
+   blockNumbers: (number | null)[][][];
 }
 
-const Fretboard: React.FC<FretboardProps> = ({currentPlayingNotes, scaleNotes, rootNote }) => {
+const Fretboard: React.FC<FretboardProps> = ({currentPlayingNotes, scaleNotes, rootNote, selectedBlock, blockNumbers }) => {
 
     return (
         <GuitarContainer>
             <OpenNotesContainer>
                 {openNotes.map((note: string, index: number) => (
-                    <Note key={index} isOpenNote>
+                    <OpenNote key={index}>
                         <div>{note.replace(/[0-9]/g, '')}</div>
-                    </Note>
+                    </OpenNote>
                 ))}
             </OpenNotesContainer>
             <FretboardContainer>
                 {fretboard.map((row: string[], rowIndex: number) => (
                     <NoteRow key={rowIndex}>
                         {row.map((note: string, colIndex: number) => {
-                            const isActive = currentPlayingNotes[rowIndex][colIndex]; // 현재 재생 중인 노트
-                            const isScaleNote = !!scaleNotes[rowIndex][colIndex];    // 스케일에 속하는 노트
+                            const isActive = currentPlayingNotes[rowIndex][colIndex];
+                            const isScaleNote = !!scaleNotes[rowIndex][colIndex];
                             const isRootNote = rootNote !== '' && !!(scaleNotes[rowIndex][colIndex]?.startsWith(rootNote)); // rootNote가 빈 문자열인 경우 처리
-                            
+                            const blockNumber = blockNumbers[rowIndex][colIndex];
+
                             return (
                                 <Note 
                                     key={colIndex + 1}  
                                     isActive={isActive}
                                     isScaleNote={isScaleNote}
                                     isRootNote={isRootNote}
+                                    selectedBlock={selectedBlock}
+                                    blockNumber={blockNumber}
                                 >
                                     <div>{note.replace(/[0-9]/g, '')}</div>
                                 </Note>
