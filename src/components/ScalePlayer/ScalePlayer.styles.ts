f13cebe7a94f09ccfd75fb2d8e94d9e2177f19fd
Eunhak Lee <112919689+Ag-crane@users.noreply.github.com>
2024-10-18 11:08:08 +0900

    design: 아이콘 UI 수정

--- src/components/ScalePlayer/ScalePlayer.styles.ts
+++ src/components/ScalePlayer/ScalePlayer.styles.ts
@@ -14,7 +14,9 @@ const ButtonContainer = styled.div`
 const Button = styled.button`
     width: 50px;
     height: 40px;
+    padding-top: 5px;
     border: #ccc 1px solid;
+    border-radius: 5px;
 `;
 
 export { Container, ButtonContainer, Button };
